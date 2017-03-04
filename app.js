57d6e3407470cce327237bdd01fcd8bceb6cb8e1
iovana <ip4g14@soton.ac.uk>
2017-03-04 14:44:51 +0000

    Not using view templates anymore
    Also moved previously created pages to public

--- app.js
+++ app.js
@@ -1,23 +1,46 @@
-var express = require('express');
-var app = express();
+var http = require("http");
+var fs = require("fs");
+var path = require("path");
+var mime = require("mime");
 
-// set the port of our application
-// process.env.PORT lets the port be set by Heroku
-var port = process.env.PORT || 8080;
+function send404(response) {
+    response.writeHead(404, {"Content-type" : "text/plain"});
+    response.write("Error 404: resource not found");
+    response.end();
+}
 
-// set the view engine to ejs
-app.set('view engine', 'ejs');
+function sendPage(response, filePath, fileContents) {
+    response.writeHead(200, {"Content-type" : mime.lookup(path.basename(filePath))});
+    response.end(fileContents);
+}
 
-// make express look in the public directory for assets (css/js/img)
-app.use(express.static(__dirname + '/public'));
+function serverWorking(response, absPath) {
+    fs.exists(absPath, function(exists) {
+        if (exists) {
+            fs.readFile(absPath, function(err, data) {
+                if (err) {
+                    send404(response)
+                } else {
+                    sendPage(response, absPath, data);
+                }
+            });
+        } else {
+            send404(response);
+        }
+    });
+}
 
-// set the home page route
-app.get('/', function(req, res) {
+var server = http.createServer(function(request, response) {
+    var filePath = false;
 
-    // ejs render automatically looks in the views folder
-    res.render('index');
+    if (request.url == '/') {
+        filePath = "public/loginpage.html";
+    } else {
+        filePath = "public" + request.url;
+    }
+
+    var absPath = "./" + filePath;
+    serverWorking(response, absPath);
 });
 
-app.listen(port, function() {
-    console.log('Our app is running on http://localhost:' + port);
-});
+var port_number = server.listen(process.env.PORT || 3000);
