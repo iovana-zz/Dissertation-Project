939dd12fc3351cf178c76c781481faa177e60a93
iovana <ip4g14@soton.ac.uk>
2017-03-05 11:31:24 +0000

    Moved gulpfile

--- gulpfile.js
+++
@@ -1,8 +0,0 @@
-var gulp = require('gulp');
-var runSeq = require('run-sequence');
-
-gulp.task('heroku:production', function(){
-  runSeq('clean', 'build', 'minify')
-});
-
-gulp.task('default', function () { console.log('Hello Gulp!') });
