87754872aff71ead149941ea406ce966efff83b9
iovana <ip4g14@soton.ac.uk>
2017-03-05 11:07:39 +0000

    Added default task to gulp

--- gulpfile.js
+++ gulpfile.js
@@ -1,6 +1,8 @@
-var gulp = require('gulp')
-var runSeq = require('run-sequence')
+var gulp = require('gulp');
+var runSeq = require('run-sequence');
 
 gulp.task('heroku:production', function(){
   runSeq('clean', 'build', 'minify')
-})
+});
+
+gulp.task('default', function () { console.log('Hello Gulp!') });
